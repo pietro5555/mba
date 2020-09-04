1307e990b96472a45e3a1a2bab5e140a7396a09c
Christian Colmenares <ccolmenaresdelgado@gmail.com>
2020-09-04 14:24:43 -0400

    Estrutura de las tablas evnets, content_events, survey_options, survey_options_response

--- app/Http/Controllers/StreamingController.php
+++ app/Http/Controllers/StreamingController.php
@@ -23,35 +23,16 @@ class StreamingController extends Controller
      */
     public function index()
     { 
-       
-        // events
+        // CREATE TABLE `mba`.`events` ( `id` INT NOT NULL AUTO_INCREMENT ,  `title` VARCHAR(255) NOT NULL ,  `date` DATETIME NOT NULL ,  `type` VARCHAR(100) NULL ,  `url_streaming` VARCHAR(255) NULL ,  `url_video` VARCHAR(255) NULL ,  `user_id` INT NULL ,  `created_at` TIMESTAMP NULL ,  `updated_at` TIMESTAMP NULL ,  PRIMARY KEY  (`id`)) ENGINE = InnoDB;
+   
 
-        //     id,
-        //     title,
-        //     fecha,
-        //     type,
-        //     url_streaming,
-        //     url_video,
-        //     user_id
+        // CREATE TABLE `mba`.`event_content` ( `id` INT NOT NULL AUTO_INCREMENT ,  `title` VARCHAR(255) NOT NULL ,  `type` VARCHAR(100) NULL ,  `url` VARCHAR(255) NULL ,  `event_id` INT NOT NULL , `created_at` TIMESTAMP NULL ,  `updated_at` TIMESTAMP NULL ,   PRIMARY KEY  (`id`)) ENGINE = InnoDB;
+   
 
-        // event_content
+        //  CREATE TABLE `mba`.`survey_options` ( `id` INT NOT NULL AUTO_INCREMENT ,  `question` TEXT NOT NULL ,  `content_event_id` INT NOT NULL ,  `created_at` TIMESTAMP NULL ,  `updated_at` TIMESTAMP NULL ,    PRIMARY KEY  (`id`)) ENGINE = InnoDB;
 
-            // id 
-            // title
-            // type
-            // url
-            // event_id
 
-        // survey_options
-            // id
-            // pregunta
-            // content_event_id
-
-        // survey_options_response
-            // id
-            // survey_options_id
-            // respueta
-            // user_id
+        //  CREATE TABLE `mba`.`survey_options_response` ( `id` INT NOT NULL AUTO_INCREMENT ,  `response` TEXT NOT NULL ,  `survey_options_id` INT NOT NULL ,  `user_id` INT NOT NULL ,  `created_at` TIMESTAMP NULL ,  `updated_at` TIMESTAMP NULL ,    PRIMARY KEY  (`id`)) ENGINE = InnoDB;
 
     
         // http://localhost:8000/?access_token=234357c92534a1238561fd35dbf36eb15b4b1bc6&token_type=bearer&expires_in=86400&state=XYZ

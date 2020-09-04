c9630e24573e06e5d7fc0efb66712a29fcca26b0
Christian Colmenares <ccolmenaresdelgado@gmail.com>
2020-09-04 14:25:33 -0400

    Estrutura de las tablas evnets, content_events, survey_options, survey_options_response.

--- app/Http/Controllers/StreamingController.php
+++ app/Http/Controllers/StreamingController.php
@@ -56,13 +56,6 @@ class StreamingController extends Controller
         //     ]
         //   ]);
 
-        // $client = new \GuzzleHttp\Client();
-        // $response = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
-
-        // echo $response->getStatusCode();
-        // echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
-        // echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
-
 	   	return view('streaming.indexstreaming')->with(compact('hola'));
     }
 

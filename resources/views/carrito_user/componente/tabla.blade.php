92d7ae59e891a34d3ae0ccc55fca197bceb77428
pietro5555 <64746224+pietro5555@users.noreply.github.com>
2020-09-29 15:35:59 -0430

    Update tabla.blade.php

--- resources/views/carrito_user/componente/tabla.blade.php
+++ resources/views/carrito_user/componente/tabla.blade.php
@@ -1,19 +1,14 @@
 <h4 class="text-white text-right mb-5"><strong><i class="fa fa-shopping-cart"></i> Carrito</strong></h4>
 
 @foreach ($items as $item)
-<div class="media mb-2">
-    <img src="{{$item->curso['img']}}" class="align-self-start mr-3" alt="curso" height="200" width="150">
+<div class="media">
+    <img src="{{$item->curso['img']}}" class="align-self-start mr-3" alt="curso" style="max-height: 200px; max-width: 150px;">
     <div class="media-body">
       <h5 class="mt-0 text-white"><strong>{{$item->curso['titulo']}}</strong></h5>
-      <p>{{$item->curso['categoria']}}</p>
-      <p class="text-white"><strong>{{$item->curso['mentor']}}</strong></p>
       <br>
       <h4 class="float-righ text-right text-white">
           <small>Costo</small> <strong>{{$item->curso['precio']}} USD</strong>
       </h4>
-      <a class="btn btn-danger" href="{{route('shopping-cart.delete', [$item->id])}}">
-        <i class="fas fa-trash"></i>
-      </a>
     </div>
   </div>
 @endforeach

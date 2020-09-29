a526a73e49b954bb2d29b5d9953fefa3c6f28b39
pietro5555 <64746224+pietro5555@users.noreply.github.com>
2020-09-29 17:56:03 -0430

    menu otro ajuste

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -14,7 +14,7 @@
                 <a class="nav-link items-header" href="{{ route('courses') }}">CURSOS</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link items-header" href="#">FXT LIVE</a>
+                <a class="nav-link items-header" href="{{route('transmisiones')}}">FXT LIVE</a>
             </li>
             @if (Auth::guest())
                 <li class="nav-item dropdown">

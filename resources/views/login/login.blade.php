e831d936c91e6f4355b43dff3bbcb2a892016580
pietro5555 <64746224+pietro5555@users.noreply.github.com>
2020-09-30 16:41:18 -0430

    Update login.blade.php

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -78,11 +78,11 @@
 
                 <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
                     <li class="nav-item" role="presentation">
-                        <a class="nav-link active color-navs" id="acceso-tab" data-toggle="tab" href="#acceso" role="tab" aria-controls="acceso" aria-selected="true" style="border-top-left-radius: 1.25rem;
+                        <a class="nav-link color-navs" id="acceso-tab" data-toggle="tab" href="#acceso" role="tab" aria-controls="acceso" aria-selected="true" style="border-top-left-radius: 1.25rem;
     border-top-right-radius: 1.25rem; padding: 12px;">Acceso</a>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <a class="nav-link color-navs" id="registro-tab" data-toggle="tab" href="#registro" role="tab" aria-controls="registro" aria-selected="false" style="border-top-left-radius: 1.25rem;
+                        <a class="nav-link active color-navs" id="registro-tab" data-toggle="tab" href="#registro" role="tab" aria-controls="registro" aria-selected="false" style="border-top-left-radius: 1.25rem;
     border-top-right-radius: 1.25rem; padding: 12px;">Registro</a>
                     </li>
 
@@ -91,7 +91,7 @@
 
                 <div class="tab-content" id="myTabContent">
                     {{-- login --}}
-                    <div class="tab-pane fade show active" id="acceso" role="tabpanel" aria-labelledby="home-tab">
+                    <div class="tab-pane fade" id="acceso" role="tabpanel" aria-labelledby="home-tab">
 
                         <form class="login-form" method="POST" action="{{ route('autenticar') }}" name="formulario">
                             {{ csrf_field() }}
@@ -139,7 +139,7 @@
                 </div>
 
                 {{-- registro--}}
-                <div class="tab-pane fade" id="registro" role="tabpanel" aria-labelledby="profile-tab">
+                <div class="tab-pane fade active show" id="registro" role="tabpanel" aria-labelledby="profile-tab">
 
 
                     <form class="login-form" method="POST" action="{{ route('autenticacion.save-register') }}" name="formulario">

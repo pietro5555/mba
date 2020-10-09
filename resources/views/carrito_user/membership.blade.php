b7314404becd675a99806a2dda685f9b2c0ae71d
Carlos Gonzalez <gonzalez2.cag@gmail.com>
2020-10-09 17:03:34 -0400

    Modificar las membresias y agregar el fondo completo en la se seccion de tu avance

--- resources/views/carrito_user/membership.blade.php
+++ resources/views/carrito_user/membership.blade.php
@@ -2,17 +2,18 @@
 
 @section('content')
 <style>
-    .color-green{
+    .color-green {
         color: #6ab742;
     }
 
-    .btn-color-green{
+    .btn-color-green {
         background: #6ab742;
     }
 
-    .bg-grey-alt{
+    .bg-grey-alt {
         background: #313335;
     }
+
 </style>
 <div class="title-page-one_course col-md border-bottom-2">
     <h6>Membresias</h6>
@@ -20,45 +21,80 @@
 </div>
 <div class="col-12 mb-2">
     <div class="row justify-content-center">
+        @auth
+        @php
+            $idmembresia = 0;
+            $price_low = 1000000000000000;
+            foreach ($membresias as $membresia) {
+                if ($membresia->id > Auth::user()->membership_id){
+                    if($price_low >= $membresia->price){
+                        $price_low = $membresia->price;
+                    }
+                }
+            }
+        @endphp
+        @endauth
         @foreach ($membresias as $membresia)
-        <div class="col-12 col-md-6 col-lg-4 mt-2">
-            <div class="card h-100 bg-grey-alt">
-                <div class="card-body text-center">
-                  <h5 class="card-title text-white">
-                      <strong>{{$membresia->name}}</strong>
-                  </h5>
-                  <p class="card-text text-white">{{$membresia->descripcion}}</p>
-                  <h4 class="card-text color-green">
-                      <strong>{{$membresia->price}} USD</strong>
-                  </h4>
-                  @auth
-                    @if (!empty(Auth::user()->membership_id))
-                        @if ((Auth::user()->membership_id +1) >= $membresia->id)
-                            @if (Auth::user()->membership_id == $membresia->id)
-                                <button class="btn btn-color-green text-white" disabled>Activa</button>    
-                            @else
+        @auth
+        @if (!empty(Auth::user()->membership_id))
+            @if ($membresia->id > Auth::user()->membership_id)
+                <div class="col-12 col-md-6 col-lg-4 mt-2">
+                    <div class="card h-100 bg-grey-alt">
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-white">
+                                <strong>{{$membresia->name}}</strong>
+                            </h5>
+                            <p class="card-text text-white">{{$membresia->descripcion}}</p>
+                            <h4 class="card-text color-green">
+                                <strong>{{$membresia->price}} USD</strong>
+                            </h4>
+                            @if ($membresia->price == $price_low)
                                 <a href="{{route('shopping-cart.store', $membresia->id)}}" class="btn btn-color-green text-white">Seleccionar Membresia</a>
-                            @endif
-                        @else
-                            <button class="btn btn-color-green text-white" disabled>Seleccionar Membresia</button>
-                        @endif
-                    @else
-                        @if ($membresia->id == 1)
+                            @else
+                                <button class="btn btn-color-green text-white" disabled>Seleccionar Membresia</button>
+                            @endif    
+                        </div>
+                    </div>
+                </div>   
+            @endif
+        @else
+            <div class="col-12 col-md-6 col-lg-4 mt-2">
+                <div class="card h-100 bg-grey-alt">
+                    <div class="card-body text-center">
+                        <h5 class="card-title text-white">
+                            <strong>{{$membresia->name}}</strong>
+                        </h5>
+                        <p class="card-text text-white">{{$membresia->descripcion}}</p>
+                        <h4 class="card-text color-green">
+                            <strong>{{$membresia->price}} USD</strong>
+                        </h4>
+                        @if ($membresia->price == $price_low)
                             <a href="{{route('shopping-cart.store', $membresia->id)}}" class="btn btn-color-green text-white">Seleccionar Membresia</a>
                         @else
                             <button class="btn btn-color-green text-white" disabled>Seleccionar Membresia</button>
                         @endif    
-                    @endif
-                  @else    
-                        @if ($membresia->id == 1)
-                            <a href="{{route('shopping-cart.store', $membresia->id)}}" class="btn btn-color-green text-white">Seleccionar Membresia</a>
-                        @else
-                        <a href="{{route('shopping-cart.store', $membresia->id)}}" class="btn btn-color-green text-white">Seleccionar Membresia</a>
-                        @endif
-                  @endauth
+                    </div>
                 </div>
-              </div>
+            </div>    
+        @endif
+        @else
+        <div class="col-12 col-md-6 col-lg-4 mt-2">
+            <div class="card h-100 bg-grey-alt">
+                <div class="card-body text-center">
+                    <h5 class="card-title text-white">
+                        <strong>{{$membresia->name}}</strong>
+                    </h5>
+                    <p class="card-text text-white">{{$membresia->descripcion}}</p>
+                    <h4 class="card-text color-green">
+                        <strong>{{$membresia->price}} USD</strong>
+                    </h4>
+
+                    <a href="{{route('shopping-cart.store', $membresia->id)}}"
+                        class="btn btn-color-green text-white">Seleccionar Membresia</a>
+                </div>
+            </div>
         </div>
+        @endauth
         @endforeach
     </div>
 </div>

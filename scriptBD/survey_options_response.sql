66f902034c0d6ded937058f844b728f822e831db
ChrisN01 <crisleivysngil@gmail.com>
2020-10-12 23:15:08 -0430

    Nuevo campos tabla survey_options_response

--- scriptBD/survey_options_response.sql
+++ scriptBD/survey_options_response.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-10-2020 a las 06:08:13
+-- Tiempo de generación: 13-10-2020 a las 05:37:09
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.3.22
 
@@ -31,6 +31,7 @@ CREATE TABLE `survey_options_response` (
   `id` int(11) NOT NULL,
   `response` text NOT NULL,
   `survey_options_id` int(11) NOT NULL,
+  `selected` int(11) NOT NULL DEFAULT 0,
   `user_id` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -40,12 +41,17 @@ CREATE TABLE `survey_options_response` (
 -- Volcado de datos para la tabla `survey_options_response`
 --
 
-INSERT INTO `survey_options_response` (`id`, `response`, `survey_options_id`, `user_id`, `created_at`, `updated_at`) VALUES
-(14, 'Probando encuesta guardada y habilitada', 33, 3, NULL, NULL),
-(15, ' respuesta 1', 33, 3, NULL, NULL),
-(16, 'Probando encuesta guardada y habilitada', 33, 3, NULL, NULL),
-(17, ' respuesta 2', 33, 3, NULL, NULL),
-(18, 'aesryeturyi78iy', 34, 3, NULL, NULL);
+INSERT INTO `survey_options_response` (`id`, `response`, `survey_options_id`, `selected`, `user_id`, `created_at`, `updated_at`) VALUES
+(14, 'Probando encuesta guardada y habilitada', 33, 0, 3, NULL, NULL),
+(15, ' respuesta 1', 33, 0, 3, NULL, NULL),
+(16, 'Probando encuesta guardada y habilitada', 33, 0, 3, NULL, NULL),
+(17, ' respuesta 2', 33, 0, 3, NULL, NULL),
+(18, 'aesryeturyi78iy', 34, 0, 3, NULL, NULL),
+(19, 'Mal', 35, 0, 3, NULL, NULL),
+(20, 'Bien', 35, 0, 3, NULL, NULL),
+(21, 'Neutral', 35, 0, 3, NULL, NULL),
+(22, 'Excelente', 35, 0, 3, NULL, NULL),
+(27, 'Bien', 35, 1, 10, '2020-10-12 17:49:12', '2020-10-12 17:49:12');
 
 --
 -- Índices para tablas volcadas
@@ -65,7 +71,7 @@ ALTER TABLE `survey_options_response`
 -- AUTO_INCREMENT de la tabla `survey_options_response`
 --
 ALTER TABLE `survey_options_response`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

c19ef695ef63aa88509f736fbf455d2e28dee6c2
licfreddymillan <lic.freddymillan@gmail.com>
2020-10-30 15:14:17 -0400

    imagen de recurso (archivos de lecciones)

--- app/Models/SupportMaterial.php
+++ app/Models/SupportMaterial.php
@@ -8,7 +8,7 @@ class SupportMaterial extends Model
 {
     protected $table = 'support_material';
 
-    protected $fillable = ['lesson_id', 'title', 'type', 'material'];
+    protected $fillable = ['lesson_id', 'title', 'type', 'material', 'image'];
 
     public function lesson(){
         return $this->belongsTo('App\Models\Lesson');

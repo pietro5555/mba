45a2792403093b0eaf3eff6be78b945f92dee0d0
pietro5555 <64746224+pietro5555@users.noreply.github.com>
2020-10-31 17:23:49 -0430

    Update step2.blade.php

--- resources/views/nosotros/step2.blade.php
+++ resources/views/nosotros/step2.blade.php
@@ -7,57 +7,118 @@
       <div id="mainSlider" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item  active ">
-        	    <div class="overlay"></div>
-        	        <img src="{{ asset('nosotros/gratis-Blog/homegratis.png') }}" class="d-block w-100" alt="...">
-        	         <div class="carousel-caption row align-items-center">
-        			    <div class="blog-title font-weight-bold">¡COMIENZA AHORA!
-                          <p style="font-size: 22px;">En este mundo tan acelerado, es la preparación lo que conduce el futuro de la vida.</p>
+              <div class="overlay"></div>
+                  <img src="{{ asset('nosotros/gratis-Blog/homegratis.png') }}" class="d-block w-100" alt="...">
+                   <div class="carousel-caption row align-items-center">
+                  <div class="blog-title font-weight-bold">ﾂ｡COMIENZA AHORA!
+                          <p style="font-size: 22px;">En este mundo tan acelerado, es la preparaciﾃｳn lo que conduce el futuro de la vida.</p>
                         </div>
-        	    </div>
-        	</div>   
+              </div>
+          </div>   
         </div>
     </div>
 </div>
 </div>
 
 
-<div class="col-12">
+<div class="col-md-12">
     <div class="section-title-landing new-courses-section-title" style="text-align: center;">Bienvenidos</div>
 
+  <div class="row" style="margin-bottom: 30px;">
+     <div class="col-md-4">
+        <a data-toggle="modal" data-target="#video1">
+       <img src="{{ asset('nosotros/fundadores/bienvenido.png') }}" class="card-img-top" alt="..." style="border: solid 0;">
+       <div class="card-body">
+            <h6 class="card-title" style="color: #2A91FF; padding-left: 0px; font-size: 22px !important; font-weight: 700!important; text-align:center;">Bienvenido</h6>
+        </div>
+        </a>
+     </div>
+     
+     
+     <div class="col-md-4">
+      <a data-toggle="modal" data-target="#video2"> 
+        <img src="{{ asset('nosotros/fundadores/pro.png') }}" class="card-img-top" alt="..." style="border: solid 0;">
+        <div class="card-body">
+            <h6 class="card-title" style="color: #2A91FF; padding-left: 0px; font-size: 22px !important; font-weight: 700!important; text-align:center;">Ser Pro</h6>
+        </div>
+        </a>
+     </div>
+     
+     
+     <div class="col-md-4">
+      <a data-toggle="modal" data-target="#video3">         
+          <img src="{{ asset('nosotros/fundadores/cambio.png') }}" class="card-img-top" alt="..." style="border: solid 0;">
+        <div class="card-body">
+            <h6 class="card-title" style="color: #2A91FF; padding-left: 0px; font-size: 22px !important; font-weight: 700!important; text-align:center;">El Cambio</h6>
+        </div>
+        </a>
+    </div>
+    
+  </div>
+</div>  
 
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel" style="margin-bottom: 40px;">
- <div class="carousel-inner">
-  <div class="carousel-item active">
-    <div class="col-md-6 offset-md-3">
-        <video src="{{ asset('nosotros/videos/introduccion.mp4') }}" controls poster="{{ asset('nosotros/nosotros/13.png') }}" style="width: 100%; height: 300px;"></video>
-      <h4 style="text-align: center; margin-top: 30px; margin-bottom: 30px; color: #fff;">Plática de Introducción</h4>
+
+
+<div class="modal fade" id="video1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Bienvenido</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="cerrarmodal()"> 
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+          
+          <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="{{ asset('nosotros/videos/introduccion.mp4') }}" allowfullscreen></iframe>
+          </div>
+
+      </div>
     </div>
   </div>
+</div>
 
-    <div class="carousel-item">
-      <div class="col-md-6 offset-md-3">
-         <video src="{{ asset('nosotros/videos/v1.mp4') }}" controls poster="{{ asset('nosotros/nosotros/09.png') }}" style="width: 100%; height: 300px;"></video>
-        <h4 style="text-align: center; margin-top: 30px; margin-bottom: 30px; color: #fff;">Promocional V1 My Business Academy Pro</h4>
-       </div>
+
+<div class="modal fade" id="video2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Ser Pro</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="cerrarmodal()"> 
+          <span aria-hidden="true">&times;</span>
+        </button>
       </div>
-
-    <div class="carousel-item">
-     <div class="col-md-6 offset-md-3">
-         <video src="{{ asset('nosotros/videos/v2.mp4') }}" controls poster="{{ asset('nosotros/nosotros/10.png') }}" style="width: 100%; height: 300px;"></video>
-        <h4 style="text-align: center; margin-top: 30px; margin-bottom: 30px; color: #fff;">Promocional V2 My Business Academy Pro</h4>
+      <div class="modal-body">
+          
+          <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="{{ asset('nosotros/videos/v1.mp4') }}" allowfullscreen></iframe>
+          </div>
+          
       </div>
     </div>
+  </div>
+</div>
 
 
-   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-     <i class="fas fa-chevron-circle-left"></i>
-     <span class="sr-only">Previous</span>
-   </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-   <i class="fas fa-chevron-circle-right"></i>
-    <span class="sr-only">Next</span>
-  </a>
-
-</div>  
+<div class="modal fade" id="video3" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">El Cambio</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="cerrarmodal()"> 
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+          
+          <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="{{ asset('nosotros/videos/v2.mp4') }}" allowfullscreen></iframe>
+          </div>
+          
+      </div>
+    </div>
+  </div>
+</div>
 
 @endsection

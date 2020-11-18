d403a5c12d12fd88c9cb55b10f17479f950d484d
pietro5555 <64746224+pietro5555@users.noreply.github.com>
2020-11-18 14:22:32 -0430

    notificacion del live

--- app/Console/Commands/CorreosUser.php
+++ app/Console/Commands/CorreosUser.php
@@ -7,11 +7,13 @@ use Carbon\Carbon;
 use App\Models\Events;
 use App\Models\Streaming\Meeting;
 use App\Models\SettingCorreo;
+use App\Models\Notification;
 use App\Models\User; 
 use DB;
 use Auth; 
 use Mail;
-use App\Models\Notification;
+
+use App\Http\Controllers\Notificaciones\CorreoController;
 
 class CorreosUser extends Command
 {
@@ -49,7 +51,7 @@ class CorreosUser extends Command
         try {
             
         $eventos = Events::where('date', '=', date('Y-m-d'))->get();
-	    $horaActual = Carbon::now()->timezone("Africa/Accra");
+        $horaActual = Carbon::now()->timezone("Africa/Accra");
             foreach ($eventos as $evento){
                 $p = $evento->date."T".$evento->time;
                 $horaEvento = new Carbon($p);
@@ -80,15 +82,19 @@ class CorreosUser extends Command
         if($event->correos == 0){
           foreach($users as $user){    
              if (!empty($plantilla->contenido)) {
-			$mensaje = str_replace('@titulo', ' '.$event->title.' ', $plantilla->contenido);
-			$mensaje = str_replace('@mentor', ' '.$mentor->display_name.' ', $mensaje);
-			$mensaje = str_replace('@fecha', ' '.$fecha.' ', $mensaje);
-			$mensaje = str_replace('@nombre', ' '.$user->display_name.' ', $mensaje);
-			Mail::send('emails.plantilla',  ['data' => $mensaje], function($msj) use ($plantilla, $user){
-				$msj->subject($plantilla->titulo);
-				$msj->to($user->user_email);
-			 });
+            $mensaje = str_replace('@titulo', ' '.$event->title.' ', $plantilla->contenido);
+            $mensaje = str_replace('@mentor', ' '.$mentor->display_name.' ', $mensaje);
+            $mensaje = str_replace('@fecha', ' '.$fecha.' ', $mensaje);
+            $mensaje = str_replace('@nombre', ' '.$user->display_name.' ', $mensaje);
+            Mail::send('emails.plantilla',  ['data' => $mensaje], function($msj) use ($plantilla, $user){
+                $msj->subject($plantilla->titulo);
+                $msj->to($user->user_email);
+             });
             }
+            
+            //agendar notificacion
+            $notifiacion = new CorreoController;
+            $notifiacion->notificar($user->ID, 'EL live va a iniciar', 'calendar', 'fas fa-video', 'Iniciar Live');
           }
           
           $event->correos = 1;
@@ -107,15 +113,19 @@ class CorreosUser extends Command
         if($event->correos == 1){
           foreach($users as $user){    
              if (!empty($plantilla->contenido)) {
-			$mensaje = str_replace('@titulo', ' '.$event->title.' ', $plantilla->contenido);
-			$mensaje = str_replace('@mentor', ' '.$mentor->display_name.' ', $mensaje);
-			$mensaje = str_replace('@fecha', ' '.$fecha.' ', $mensaje);
-			$mensaje = str_replace('@nombre', ' '.$user->display_name.' ', $mensaje);
-			Mail::send('emails.plantilla',  ['data' => $mensaje], function($msj) use ($plantilla, $user){
-				$msj->subject($plantilla->titulo);
-				$msj->to($user->user_email);
-			 });
+            $mensaje = str_replace('@titulo', ' '.$event->title.' ', $plantilla->contenido);
+            $mensaje = str_replace('@mentor', ' '.$mentor->display_name.' ', $mensaje);
+            $mensaje = str_replace('@fecha', ' '.$fecha.' ', $mensaje);
+            $mensaje = str_replace('@nombre', ' '.$user->display_name.' ', $mensaje);
+            Mail::send('emails.plantilla',  ['data' => $mensaje], function($msj) use ($plantilla, $user){
+                $msj->subject($plantilla->titulo);
+                $msj->to($user->user_email);
+             });
             }
+            
+            //agendar notificacion
+            $notifiacion = new CorreoController;
+            $notifiacion->notificar($user->ID, 'EL live ya a iniciado', 'calendar', 'fas fa-video', 'Live inicio');
           }
           
           $event->correos = 2;
